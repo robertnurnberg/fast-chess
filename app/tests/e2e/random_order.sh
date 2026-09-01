4cff2649fddeb9da53f6c32e8c43e971ea4747e4
Disservin <disservin.social@gmail.com>
2026-09-01 19:20:51 +0200

    test: update srand in e2e

--- app/tests/e2e/random_order.sh
+++ app/tests/e2e/random_order.sh
@@ -12,13 +12,15 @@ rm -f random_run1.pgn random_run2.pgn
 output_file=$(mktemp)
 ./fastchess -engine cmd=./random_mover name=random_move_1 -engine cmd=./random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 3 -repeat -concurrency 1 \
-    -openings file=./app/tests/data/short.epd format=epd order=random -pgnout file=random_run1.pgn -log file=log.txt level=info 2>&1 | tee "$output_file"
+    -openings file=./app/tests/data/short.epd format=epd order=random -srand 1 \
+    -pgnout file=random_run1.pgn -log file=log.txt level=info 2>&1 | tee "$output_file"
 check_no_engine_runtime_errors "$output_file"
 
 output_file=$(mktemp)
 ./fastchess -engine cmd=./random_mover name=random_move_1 -engine cmd=./random_mover name=random_move_2 \
     -each tc=2+0.02s -rounds 3 -repeat -concurrency 1 \
-    -openings file=./app/tests/data/short.epd format=epd order=random -pgnout file=random_run2.pgn -log file=log.txt level=info 2>&1 | tee "$output_file"
+    -openings file=./app/tests/data/short.epd format=epd order=random -srand 2 \
+    -pgnout file=random_run2.pgn -log file=log.txt level=info 2>&1 | tee "$output_file"
 check_no_engine_runtime_errors "$output_file"
 
 fens_run1=$(grep -A1 "^\[SetUp" random_run1.pgn | grep "^\[FEN" | head -6)
